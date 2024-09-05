b4196f8a62887aa9833bbc7425f20180e12e57e3
dimos <dimos86m@gmail.com>
2024-09-05 21:18:20 +0300

    Remove logo, link, and related styles from App component

--- src/App.tsx
+++ src/App.tsx
@@ -71,10 +71,6 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
                 <div className="Chessboard-container">
                     <Chessboard
                         position={chess.fen()}
@@ -84,14 +80,6 @@ function App() {
                         onSquareClick={handleSquareClick}
                     />
                 </div>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React {chess.fen()}
-                </a>
             </header>
         </div>
     );
