ab07a18e10fb5d10c65dc4a09a30da0c28716bc8
dimos <dimos86m@gmail.com>
2024-09-05 15:23:09 +0300

    Add chessboard integration with chess.js to App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Chess } from 'chess.js';
+import { Chessboard } from 'react-chessboard';
+
 function App() {
+  const chess = new Chess();
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -10,13 +15,16 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <div className="Chessboard-container">
+          <Chessboard position={chess.fen()} />
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React {chess.fen()}
         </a>
       </header>
     </div>
